9e36b52405b03158b91013a85a04f12cf56a386d
Vineet Kumar <vineet.kumar@bsc.es>
2021-03-23 20:32:23 +0100

    Updated PreferPredicateWithVPIntrinsics switch to be able to indicate different approaches

--- llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
+++ llvm/lib/Transforms/Vectorize/LoopVectorize.cpp
@@ -235,10 +235,53 @@ static cl::opt<PreferPredicateTy::Option> PreferPredicateOverEpilogue(
                          "prefers tail-folding, don't attempt vectorization if "
                          "tail-folding fails.")));
 
-static cl::opt<bool> PreferPredicateWithVPIntrinsics(
-    "prefer-predicate-with-vp-intrinsics", cl::init(false), cl::Hidden,
+// Option prefer-predicate-with-vp-intrinsics is an experimental switch to
+// indicate that the loop vectorizer should try to generate VP intrinsics if
+// tail-folding is enabled (note that this option is dependent on the
+// prefer-predicate-over-epilogue option being set to predicate-dont-vectorize).
+// This can be particularly useful for targets like RISC-V and SX-Aurora that
+// support vector length predication.
+// Currently this switch takes four possible values:
+// 0. no-predication: Do not generate VP intrinsics.
+// 1. if-active-vector-length-supported: Only generate VP intrinsics if the
+// target supports active vector length based predication.
+// 2. without-avl-support: Generate VP intriniscs even if vector length based
+// predication is not supported. This will behave a bit like existing
+// tail-folding by using a mask for predication, except all instructions are
+// widened to VP intrinsics and not just memory instructions. Use of this
+// options is discouraged and is only meant for experimental/testing purpose.
+// 3. force-active-vector-length-support: This is purely an experimental/testing
+// option which will be removed in future. It forces the loop vectorizer to
+// assume that the target supports vector length predication.
+namespace PreferVPIntrinsicsTy {
+enum Option {
+  NoPredication = 0,
+  IfAVLSupported,
+  WithoutAVLSupport,
+  ForceAVLSupport
+};
+} // namespace PreferVPIntrinsicsTy
+
+static cl::opt<PreferVPIntrinsicsTy::Option> PreferPredicateWithVPIntrinsics(
+    "prefer-predicate-with-vp-intrinsics",
+    cl::init(PreferVPIntrinsicsTy::NoPredication), cl::Hidden,
     cl::desc("When vectorizing with tail-folding, generate vector predication "
-             "intrinsics."));
+             "intrinsics."),
+    cl::values(
+        clEnumValN(PreferVPIntrinsicsTy::NoPredication, "no-predication",
+                   "Do not generate VP intrinsics."),
+        clEnumValN(PreferVPIntrinsicsTy::IfAVLSupported,
+                   "if-active-vector-length-supported",
+                   "Only generate VP intrinsics if the target supports vector "
+                   "length predication."),
+        clEnumValN(PreferVPIntrinsicsTy::WithoutAVLSupport,
+                   "without-active-vector-length-support",
+                   "Generate VP intrinsics even if vector length predication "
+                   "is not supported. This option is discouraged."),
+        clEnumValN(PreferVPIntrinsicsTy::ForceAVLSupport,
+                   "force-active-vector-length-support",
+                   "Assume that the target supports vector length predication "
+                   "and generate VP intrinsics accordingly.")));
 
 static cl::opt<bool> MaximizeBandwidth(
     "vectorizer-maximize-bandwidth", cl::init(false), cl::Hidden,
@@ -2997,8 +3040,7 @@ void InnerLoopVectorizer::vectorizeMemoryInstruction(
           NewSI = Builder.CreateIntrinsic(
               Intrinsic::vp_store, {StoredValTy, VecPtr->getType()},
               {StoredVal, VecPtr, Builder.getInt32(Alignment.value()),
-               BlockInMaskPart, EVLPartI32},
-              "vp.op.store");
+               BlockInMaskPart, EVLPartI32});
         } else if (isMaskRequired) {
           NewSI = Builder.CreateMaskedStore(StoredVal, VecPtr, Alignment,
                                             BlockInMaskParts[Part]);
@@ -3043,7 +3085,7 @@ void InnerLoopVectorizer::vectorizeMemoryInstruction(
             {VecPtr->getType()->getPointerElementType(), VecPtr->getType()},
             {VecPtr, Builder.getInt32(Alignment.value()), BlockInMaskPart,
              EVLPartI32},
-            "vp.op.load");
+            nullptr, "vp.op.load");
       } else if (isMaskRequired) {
         NewLI = Builder.CreateMaskedLoad(
             VecPtr, Alignment, BlockInMaskParts[Part], PoisonValue::get(DataTy),
@@ -5774,16 +5816,28 @@ LoopVectorizationCostModel::computeMaxVF(ElementCount UserVF, unsigned UserIC) {
   // FIXME: look for a smaller MaxVF that does divide TC rather than masking.
   if (Legal->prepareToFoldTailByMasking()) {
     FoldTailByMasking = true;
-    if (PreferPredicateWithVPIntrinsics) {
-      if (UserIC < 2) {
-        PreferVPIntrinsics = true;
-        LLVM_DEBUG(dbgs() << "LV: Preference for VP intrinsics indicated. Will "
-                             "try to generate VP Intrinsics.\n");
-      } else {
-        LLVM_DEBUG(dbgs() << "LV: Will not generate VP intrinsics since "
-                             "interleave count specified is greater than 1.\n");
-      }
+    if (!PreferPredicateWithVPIntrinsics)
+      return MaxVF;
+
+    if (UserIC > 1) {
+      LLVM_DEBUG(dbgs() << "LV: Preference for VP intrinsics indicated. Will "
+                           "not generate VP intrinsics since interleave count "
+                           "specified is greater than 1.\n");
+      return MaxVF;
     }
+
+    if (PreferPredicateWithVPIntrinsics ==
+        PreferVPIntrinsicsTy::IfAVLSupported) {
+      PreferVPIntrinsics = TTI.hasActiveVectorLength();
+      LLVM_DEBUG(dbgs() << "LV: Preference for VP intrinsics indicated. Will "
+                           "try to generate VP Intrinsics if the target "
+                           "support vector length predication.\n");
+    } else {
+      PreferVPIntrinsics = true;
+      LLVM_DEBUG(dbgs() << "LV: Preference for VP intrinsics indicated. Will "
+                           "try to generate VP Intrinsics.\n");
+    }
+
     return MaxVF;
   }
 
@@ -9605,14 +9659,23 @@ void VPPredicatedWidenMemoryInstructionRecipe::execute(
 }
 
 Value *InnerLoopVectorizer::createEVL() {
+  assert(PreferPredicateWithVPIntrinsics !=
+             PreferVPIntrinsicsTy::NoPredication &&
+         "Predication with VP intrinsics turned off.");
+
+  if (PreferPredicateWithVPIntrinsics == PreferVPIntrinsicsTy::IfAVLSupported)
+    assert(TTI->hasActiveVectorLength() &&
+           "Target does not support vector length predication.");
+
   auto *MinVF = Builder.getInt32(VF.getKnownMinValue());
   Value *RuntimeVL =
       VF.isScalable() ? Builder.CreateVScale(MinVF, "vscale.x.vf") : MinVF;
-  // FIXME: If the target does not support active vector length, the use of VP
-  // intrinsics for it is discouraged. We should decide against using vector
-  // length based predication before creating the VPlan.
-  if (!TTI->hasActiveVectorLength())
+
+  if (PreferPredicateWithVPIntrinsics ==
+          PreferVPIntrinsicsTy::WithoutAVLSupport &&
+      !TTI->hasActiveVectorLength()) {
     return RuntimeVL;
+  }
 
   Value *Remaining = Builder.CreateSub(TripCount, Induction);
   // FIXME: This is a proof-of-concept naive implementation to demonstrate using
